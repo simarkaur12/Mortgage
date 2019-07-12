ee12510cd8a42b9979bf58d8c472874bfa88e032
Harsimar <simarkaur1295@gmail.com>
2019-07-12 10:37:17 +0530

    Initial commit

---
+++ src/main/java/com/hcl/mortgage/MortgageApplication.java
@@ -0,0 +1,14 @@
+package com.hcl.mortgage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortgageApplication {
+
+	public static void main(String[] args) {
+		System.out.println("1111111111111");
+		SpringApplication.run(MortgageApplication.class, args);
+	}
+
+}
